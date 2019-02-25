ffdb77d01c8816cf5ca83a3904e44ce27de16b33
Kevin Yang <kv7870@gmail.com>
2019-02-25 02:07:54 -0500

    4

--- Nifty_Mountains/header.h
+++ Nifty_Mountains/header.h
@@ -8,6 +8,9 @@ int readElevation(apmatrix<int> &elevation);
 int findMin(const apmatrix<int> &elevation);
 int findMax(const apmatrix<int> &elevation);
 void drawMap(const int &min, const int &max, const apmatrix<int> &elevation, ALLEGRO_DISPLAY * display);
-void drawLowestElevPath(const apmatrix<int> &elevation, vector<int> &shortestPath, int &minTotalChange);
-int indexOfLowestElevPath(const vector<int> &shortestPath, const int &minTotalChange);
+int drawLowestElevPath(const apmatrix<int> &elevation, vector<int> &shortestPath, int &minTotalChange,
+	int currRow, int * jIndex, int jDim, vector<int> &currentPath); 
+int indexOfLowestElevPath(const vector<int> &shortestPath, const int &minTotalChange, int rMin);
 void drawBestPath(const int &startRow, const vector<int> &shortestPath);
+void levelThree(const int &min, const int &max, const apmatrix<int> &elevation, ALLEGRO_DISPLAY * display); 
+void levelFour_1(const int &min, const int &max, const apmatrix<int> &elevation, ALLEGRO_DISPLAY * display); 
